b59e85331c10776b606d333d9ec418094a998deb
Nathan Cruz <nathan.cruz@unifesp.br>
2016-07-17 18:48:24 -0300

    Removido link para atom-feed.

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,6 +11,7 @@ DEFAULT_LANG = 'pt'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
+FEED_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
